2a2e19b0b9c4c78f91eb1417e3e8dff4913bbc73
galter <ehgalter@gmail.com>
2021-03-07 11:26:38 -0300

    Fixed all bugs following Test Suites

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((resp) => {
+      setRepositories(resp.data);
+    });
+
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      title: "Desafio ReactJS",
+      url: "htpps://github.com/josepholiveira",
+      techs: ["React", "NodeJS"]
+    };
+
+    await api.post('repositories', data).then((resp) => {
+      setRepositories([...repositories, resp.data]);
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repoWithoutDeleted = repositories.filter(repo => repo.id !== id);
+
+    setRepositories(repoWithoutDeleted);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories && repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
